2ed2e84eb1e4522d5d2881b9aa91d2853898a72b
srinjoy2954 <srinjoy.royiembca2025@gmail.com>
2024-11-09 23:32:59 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://earthquake-prediction-using-ml.onrender.com';
 
 export const predictEarthquakes = async (dateTime) => {
   try {
